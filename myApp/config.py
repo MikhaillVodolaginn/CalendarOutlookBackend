582d19a4e9507eca0d5b8a49edd4c8c21ad7b4fa
MikhaillVodolaginn <103418305+MikhaillVodolaginn@users.noreply.github.com>
2022-12-24 19:29:37 +0500

    Update config.py

--- myApp/config.py
+++ myApp/config.py
@@ -1,6 +1,6 @@
-CLIENT_ID = "4d76c578-e336-4d69-ae97-7489722340d2"
+CLIENT_ID = ""
 
-CLIENT_SECRET = "zth8Q~8rfrO8gNHmnVSXEJWq~EEpLMkfajYRpdvB"
+CLIENT_SECRET = ""
 
 AUTHORITY = "https://login.microsoftonline.com/common"
 
