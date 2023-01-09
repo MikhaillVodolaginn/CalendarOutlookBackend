cefd72561afabf023e7a9d7330512de0efe0550f
mihail <mikhailvodolagin@icloud.com>
2023-01-10 02:08:27 +0500

    Commit

--- calendarApp/models.py
+++ calendarApp/models.py
@@ -5,9 +5,16 @@ from msal import PublicClientApplication
 
 
 class Config:
-    CLIENT_ID = ''
+    CLIENT_ID = '4d76c578-e336-4d69-ae97-7489722340d2'
     AUTHORITY = 'https://login.microsoftonline.com/common'
     SCOPE = ['User.Read', 'Calendars.ReadWrite']
     APP = PublicClientApplication(client_id=CLIENT_ID, authority=AUTHORITY)
     FLOW = APP.initiate_device_flow(scopes=SCOPE)
     ACCESS_TOKEN = ''
+
+
+class Calendars:
+    orange = {'name': 'Оранжевая переговорка', 'id': ''}
+    green = {'name': 'Зеленая переговорка', 'id': ''}
+    red = {'name': 'Красная переговорка', 'id': ''}
+    yellow = {'name': 'Желтая переговорка', 'id': ''}
--- calendarApp/views.py
+++ calendarApp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from datetime import datetime, timedelta
 import requests
-from .models import Config
+from .models import Config, Calendars
 config = Config()
+calendars = Calendars()
 
 
 def index(request):
@@ -17,26 +18,42 @@ def calendar(request):
         app = config.APP
         flow = config.FLOW
         config.ACCESS_TOKEN = app.acquire_token_by_device_flow(flow=flow)['access_token']
+        GetAllCalendars({'Authorization': 'Bearer ' + config.ACCESS_TOKEN})
 
-    headers = {'Authorization': 'Bearer ' + config.ACCESS_TOKEN}
-    context = GetCalendarThisWeak(headers)
+    context = GetCalendarThisWeak({'Authorization': 'Bearer ' + config.ACCESS_TOKEN, 'Prefer': 'outlook.timezone="Asia/Yekaterinburg"'})
     return render(request, 'calendar.html', context)
 
 
-def GetCalendarThisWeak(headers):
+def GetAllCalendars(headers):
+    response = requests.get(f"https://graph.microsoft.com/v1.0/me/calendars", headers=headers)
+    print(response.ok)
+    for item in response.json()["value"]:
+        if item["name"] == calendars.orange['name']:
+            calendars.orange["id"] = item["id"]
+        elif item["name"] == calendars.green['name']:
+            calendars.green["id"] = item["id"]
+        elif item["name"] == calendars.red['name']:
+            calendars.red["id"] = item["id"]
+        elif item["name"] == calendars.yellow['name']:
+            calendars.yellow["id"] = item["id"]
+
+
+def GetCalendarThisWeak(headers, room=calendars.orange):
     now = datetime.fromordinal(datetime.now().toordinal())
     weak_day = datetime.weekday(now)
     start_datatime = now - timedelta(days=weak_day)
     end_datetime = now + timedelta(days=7 - weak_day)
+    room_id = room['id']
     response = requests.get(
-        f"https://graph.microsoft.com/v1.0/me/calendarview?startdatetime={start_datatime.isoformat()}&enddatetime={end_datetime.isoformat()}",
+        f"https://graph.microsoft.com/v1.0/me/calendars/{room_id}/calendarView?startdatetime={start_datatime.isoformat()}&enddatetime={end_datetime.isoformat()}",
         headers=headers)
-    output = GetOutputJSON(response.json(), start_datatime)
+    print(response.ok)
+    output = GetOutputJSON(response.json(), start_datatime, room)
     return output
 
 
-def GetOutputJSON(content, start_datatime):
-    output_json = {"name": "Оранжевая переговорка", "calendar": []}
+def GetOutputJSON(content, start_datatime, room):
+    output_json = {"name": room['name'], "calendar": []}
     for i in range(0, 7):
         current_day = start_datatime + timedelta(days=i)
         current_day_str = str(current_day).partition(' ')[0]
