ddb7ba8a237bef73928e0f6c27c286c04cd24698
mihail <mikhailvodolagin@icloud.com>
2023-01-10 12:57:04 +0500

    Commit

--- calendarApp/models.py
+++ calendarApp/models.py
@@ -5,7 +5,7 @@ from msal import PublicClientApplication
 
 
 class Config:
-    CLIENT_ID = '4d76c578-e336-4d69-ae97-7489722340d2'
+    CLIENT_ID = '4d76c578-e336-4d69-ae97-7489722340d2'  # your application's CLIENT_ID
     AUTHORITY = 'https://login.microsoftonline.com/common'
     SCOPE = ['User.Read', 'Calendars.ReadWrite']
     APP = PublicClientApplication(client_id=CLIENT_ID, authority=AUTHORITY)
